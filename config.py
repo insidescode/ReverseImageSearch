ee6c4e8bf8eb1ba0fc6fe7587e7496299904a5ae
insidejob <84038103+insidescode@users.noreply.github.com>
2022-03-02 13:43:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config(object):
-    CLIENT_ID = "f2ccabc29a76d62"
-    BOT_TOKEN = "5274671619:AAHSddCzHNGHGPbGS0_agWlrGUHaB5xp7As"
+    CLIENT_ID = f2ccabc29a76d62
+    BOT_TOKEN = 5274671619:AAHSddCzHNGHGPbGS0_agWlrGUHaB5xp7As
     HEROKU_APP_NAME = ""
